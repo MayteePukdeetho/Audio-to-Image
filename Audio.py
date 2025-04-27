a73d0157a8bb6ce0c4823dbb494102594fdeac69
Mayteetad Pukdeetho <mpukdeetho@ufl.edu>
2025-04-27 14:16:00 -0400

    changes before i take a nap. i dont feel too well.

--- Audio.py
+++ Audio.py
@@ -1,3 +1,5 @@
+from wsgiref.util import request_uri
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -29,23 +31,44 @@ def audio_to_image(list_of_pathnames):
 
 
 def audio_to_text(list_of_pathnames):
+    returned_words = []
     model = whisper.load_model("base")
     for i in range(len(list_of_pathnames)):
-        result = model.transcribe(list_of_pathnames[i])
-        print(result["text"])
-
+        result = model.transcribe(list_of_pathnames[i], word_timestamps= True)
+        for segment in result["segments"]:
+            words = segment.get("words", [])
+            for word_data in words:
+                word = word_data['word']
+                start = word_data['start']
+                end = word_data['end']
+                returned_words.append(word)
+        return returned_words
 
 
 def audio_to_face(list_of_pathnames):
-    default_face = plt.plot([-32,0], [32,0])
+    words_to_draw = audio_to_text(list_of_pathnames)
+    fig, ax = plt.subplots()
+    x = np.linspace(-32, 32, 400)
+    for word in words_to_draw:
+        if "A" in word:
+            y = (5 / 1024) * x ** 2
+            y_straight = (np.full_like(x, 5))
+            plt.plot(x, y)
+            plt.plot(x, y_straight)
+            plt.fill_between(x, y, y_straight, color='blue')
+        elif "O" in word:
+            plt.plot()
+        elif "U" in word:
+            plt.plot()
+        elif ("E" or "I") in word:
+            plt.plot()
+        else:
+            output_plot = plt.plot([-32,0], [32,0])
+        ax.axis('off')
+        ax.set_aspect('equal')
+        plt.savefig(f"output/frame{i}.png", pad_inches=0, bbox_inches='tight', transparent=True)
+        plt.close()
 
 
-model = whisper.load_model("base")
-result = model.transcribe("data/test.wav", word_timestamps=True)
-for segment in result["segments"]:
-    words = segment.get("words", [])
-    for word_data in words:
-        word = word_data['word']
-        start = word_data['start']
-        end = word_data['end']
-        print(word, start, end)
+
+
